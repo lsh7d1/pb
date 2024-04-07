d4191f28cb2a9f8fc73cf1eac33655a9a2912f6b
bish <lsh7d1@163.com>
2024-04-07 11:36:33 +0800

    feat: delete http interface

--- gen.sh
+++ gen.sh
@@ -4,7 +4,5 @@ API_PROTO_FILES=$(find api -name "*.proto")
 # 使用protoc编译器编译协议缓冲区文件
 protoc --proto_path=./api \
         --go_out=paths=source_relative:./api \
-        --go-http_out=paths=source_relative:./api \
         --go-grpc_out=paths=source_relative:./api \
-        --openapi_out=paths=source_relative:. \
         ${API_PROTO_FILES}
